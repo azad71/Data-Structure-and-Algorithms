f368796dd25dd0476e4a83bcdd186935f38bac04
azad71 <mdmamun732@gmail.com>
2020-04-13 21:44:47 +0600

    AVL tree is implemented

--- Tree/AVL_tree.cpp
+++ Tree/AVL_tree.cpp
@@ -1,62 +1,155 @@
-// implementation of binary search tree
+/*
+    implementation of AVL tree
+    An special implementation of binary search tree that balance itself
+    automatically in every insertion and deletion
 
+*/
 #include<bits/stdc++.h>
 
 using namespace std;
 
 struct Node {
-    int data;
     Node* left;
+    int data;
+    int height;
     Node* right;
 };
 
 Node* root = NULL;
 
-Node* add_new_node(int item) {
-    Node* new_node = new Node();
-    new_node->data = item;
-    new_node->left = NULL;
-    new_node->right = NULL;
+// returns the depth of a node
+int get_height(Node* p) {
+    if(p == NULL)
+        return 0;
+    int x = get_height(p->left);
+    int y = get_height(p->right);
 
-    return new_node;
+    return x > y ? x+1: y+1;
 }
 
-void insert_node(int item) {
-    Node* temp = root;
-    Node* new_node = add_new_node(item);
-    Node* tail_node = new Node();
-
-    // check if tree is empty
-    if(root == NULL) {
-        root = new_node;
-        cout<<endl<<item<<" is added"<<endl;
-        return;
-    }
-
-    while(temp != NULL) {
-        tail_node = temp;
-
-        if(item < temp->data) {
-            temp = temp->left;
-        } else if(item > temp->data) {
-            temp = temp->right;
-        } else {
-            // this means node is already exist in tree
-            cout<<endl<<item<<" already exist in the tree"<<endl;
-            return;
-        }
-    }
-
-    // we made this far...that means item isn't inserted before
-    // now link the newly created node with tail node
-    if(item < tail_node->data) {
-        tail_node->left = new_node;
-    } else {
-        tail_node->right = new_node;
-    }
-    cout<<endl<<item<<" is added"<<endl;
+int balance_factor(Node* p) {
+    return get_height(p->left) - get_height(p->right);
 }
 
+Node* LL_rotation(Node* p) {
+
+    Node* pl = new Node();
+    Node* plr = new Node();
+
+    pl = p->left;
+    plr = pl->right;
+
+    pl->right = p;
+    p->left = plr;
+    p->height = get_height(p);
+    pl->height= get_height(pl);
+
+    if(root == p)
+        root = pl;
+
+    return pl;
+}
+
+Node* LR_rotation(Node* p) {
+    Node* pl = new Node();
+    Node* plr = new Node();
+
+    pl = p->left;
+    plr = pl->right;
+
+    pl->right = plr->left;
+    p->left = plr->right;
+    plr->right = p;
+    plr->left = pl;
+
+    pl->height = get_height(pl);
+    p->height = get_height(p);
+    plr->height = get_height(plr);
+
+    if(root == p)
+        root = plr;
+
+    return plr;
+
+}
+
+Node* RR_rotation(Node* p) {
+    Node* pr = new Node();
+    Node* prl = new Node();
+
+    pr = p->right;
+    prl = pr->left;
+
+    p->right = prl;
+    pr->left = p;
+    p->height = get_height(p);
+    pr->height = get_height(pr);
+
+    if (root == p)
+        root = pr;
+
+    return pr;
+
+}
+
+Node* RL_rotation(Node* p) {
+    Node* pr = new Node();
+    Node* prl = new Node();
+
+    pr = p->right;
+    prl = pr->left;
+
+    p->right = prl->left;
+    pr->left = prl->right;
+    prl->left = p;
+    prl->right = pr;
+
+    p->height = get_height(p);
+    pr->height = get_height(pr);
+    prl->height = get_height(prl);
+
+    if(root == p)
+        root = prl;
+
+    return prl;
+}
+
+
+// inserting node, this time we are doing it with recursion
+Node* insert_node(Node* p, int key) {
+
+    // base case
+    if(p == NULL) {
+        Node* new_node = new Node();
+        new_node->data = key;
+        new_node->height = 1;
+        new_node->left = NULL;
+        new_node->right = NULL;
+        cout<<endl<<key<<" is inserted in tree"<<endl;
+        return new_node;
+    }
+
+    // here's the sauce for recursion
+    if(key < p->data) {
+        p->left = insert_node(p->left, key);
+    } else if(key > p->data) {
+        p->right = insert_node(p->right, key);
+    }
+
+    p->height = get_height(p);
+
+    // adjusting balance
+    if(balance_factor(p) == 2 && balance_factor(p->left) == 1) // heavy on the left then left side
+        return LL_rotation(p);
+    else if(balance_factor(p) == 2 && balance_factor(p->left) == -1) // heavy on the left then right
+        return LR_rotation(p);
+    else if(balance_factor(p) == -2 && balance_factor(p->right) == -1) // heavy on the right then right
+        return RR_rotation(p);
+    else if(balance_factor(p) == -2 && balance_factor(p->right) == 1) // heavy on the right then left
+        return RL_rotation(p);
+
+    return p;
+}
 void find_item(int item) {
     Node* temp = new Node();
     temp = root;
@@ -73,16 +166,6 @@ void find_item(int item) {
     }
 
     cout<<endl<<item<<" is not found in the tree"<<endl;
-}
-
-// returns the height of both side
-int height(Node* p) {
-    if(p == NULL)
-        return 0;
-    int x = height(p->left);
-    int y = height(p->right);
-
-    return x > y ? x+1: y+1;
 }
 
 // returns rightmost leaf node of left side
@@ -103,8 +186,12 @@ Node* successor(Node* p) {
     return p;
 }
 
+// same as binary search tree deletion
+// successor or predecessor takes the place of deleted node
+// then adjust the balance
 Node* delete_node(Node* p, int item) {
     Node* q;
+
     if(p == NULL) {
         cout<<endl<<item<<" doesn't exist in the tree"<<endl;
         return NULL;
@@ -123,7 +210,7 @@ Node* delete_node(Node* p, int item) {
     } else if(item > p->data) {
         p->right = delete_node(p->right, item);
     } else {
-        if(height(p->left) > height(p->right)) {
+        if(get_height(p->left) > get_height(p->right)) {
             q = predecessor(p->left);
             p->data = q->data;
             p->left = delete_node(p->left, q->data);
@@ -133,6 +220,18 @@ Node* delete_node(Node* p, int item) {
             p->right = delete_node(p->right, q->data);
         }
     }
+
+    p->height = get_height(p);
+
+    // adjusting balance
+    if(balance_factor(p) == 2 && balance_factor(p->left) == 1) // heavy on the left then left side
+        return LL_rotation(p);
+    else if(balance_factor(p) == 2 && balance_factor(p->left) == -1) // heavy on the left then right
+        return LR_rotation(p);
+    else if(balance_factor(p) == -2 && balance_factor(p->right) == -1) // heavy on the right then right
+        return RR_rotation(p);
+    else if(balance_factor(p) == -2 && balance_factor(p->right) == 1) // heavy on the right then left
+        return RL_rotation(p);
     return p;
 }
 
@@ -160,13 +259,14 @@ void postorder_traversal(Node* root) {
     }
 }
 
-
-// driver code
-
 int main() {
 
     int choice;
 
+    int temp;
+    cout<<endl<<"Enter a root node first: ";
+    cin>>temp;
+    root = insert_node(root, temp);
 
     cout<<endl<<"Enter below numeric value for listed operation\n\n";
 
@@ -189,7 +289,7 @@ int main() {
                 int item;
                 cout<<"\nEnter item to be inserted: ";
                 cin>>item;
-                insert_node(item);
+                insert_node(root, item);
                 break;
             }
         case 2:
